6a6d71bcec0025233f052b556ca3ca9ab49a1a40
monroesummer <rustamasylgareev@gmail.com>
2017-04-04 00:34:55 +0300

    Initial Commit

---
+++ src/AliMine/Controllers/DatabaseController.cs
@@ -0,0 +1,107 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AliMine.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AliMine.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        AliMineContext db;
+        public DatabaseController(AliMineContext context)
+        {
+            db = context;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Categories()
+        {
+            IQueryable<Category> categories = db.Categories;
+
+            return View(await categories.AsNoTracking().ToListAsync());
+        }
+
+        [Authorize]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(Category category)
+        {
+
+            db.Categories.Add(category);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Categories");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CategoryDetails(int? id)
+        {
+            if (id != null)
+            {
+                Category category = await db.Categories.FirstOrDefaultAsync(p => p.Id == id);
+                if (category != null)
+                    return View(category);
+            }
+            return NotFound();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> EditCategory(int? id)
+        {
+            if (id != null)
+            {
+                Category category = await db.Categories.FirstOrDefaultAsync(p => p.Id == id);
+                if (category != null)
+                    return View(category);
+            }
+            return NotFound();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> EditCategory(Category category)
+        {
+            db.Categories.Update(category);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Categories");
+        }
+
+        [Authorize]
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDeleteCategory(int? id)
+        {
+            if (id != null)
+            {
+                Category category = await db.Categories.FirstOrDefaultAsync(p => p.Id == id);
+                if (category != null)
+                    return View(category);
+            }
+            return NotFound();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> DeleteCategory(int? id)
+        {
+            if (id != null)
+            {
+                Category category = await db.Categories.FirstOrDefaultAsync(p => p.Id == id);
+                if (category != null)
+                {
+                    db.Categories.Remove(category);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Categories");
+                }
+            }
+            return NotFound();
+        }
+
+    }
+}
---
+++ src/AliMine/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace AliMine.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImgAlias { get; set; }
+
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
